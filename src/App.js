d39148a74d10b54cea58ff0a84c9d9090b7c0772
Connor Yager <csy7ay@virginia.edu>
2021-04-25 15:31:45 -0400

    switched to hashRouter

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Projects from './components/Projects';
 import Project from './components/Project';
 import Footer from './components/Footer';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
