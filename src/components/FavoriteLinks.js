290739e1be193554b7d56c3c737780c189ebd085
Connor Yager <csy7ay@virginia.edu>
2025-11-17 10:37:22 -0500

    update favorite links

--- src/components/FavoriteLinks.js
+++ src/components/FavoriteLinks.js
@@ -10,14 +10,17 @@ function FavoriteLinks() {
             <h3>Connor's Favorite Links</h3>
             <hr />
             <ul className="favorite-links-ul">
-				<li>
-					<a href="https://soback.connoryager.com/" target="_blank" rel="noreferrer">We're so back game <FontAwesomeIcon icon={faExternalLinkAlt} /></a>
-				</li>
+		<li>
+			<a href="https://soback.connoryager.com/" target="_blank" rel="noreferrer">We're so back game <FontAwesomeIcon icon={faExternalLinkAlt} /></a>
+		</li>
+	    	<li>
+	    		<a href="https://empire.connoryager.com/" target="_blank" rel="noreferrer">Empire game <FontAwesomeIcon icon={faExternalLinkAlt} /></a>
+	    	</li>
                 <li>
                     <a href="https://www.reddit.com/r/UVA/comments/wtmgwr/from_2000_feet_above_grounds/?utm_source=share&utm_medium=ios_app&utm_name=iossmf" target="_blank" rel="noreferrer">View from 2000 feet above UVA  <FontAwesomeIcon icon={faExternalLinkAlt} /></a>
                 </li>
                 <li>
-                    <a href="https://www.craiyon.com/" target="_blank" rel="noreferrer">Craiyon - "AI model drawing images from any prompt" <FontAwesomeIcon icon={faExternalLinkAlt} /></a>
+                    <a href="https://www.nytimes.com/interactive/2023/upshot/extremely-detailed-nyc-neighborhood-map.html" target="_blank" rel="noreferrer"> New York Times&#39; Extremely Detailed Neighborhood Map <FontAwesomeIcon icon={faExternalLinkAlt} /></a>
                 </li>
                 <li>
                     <a href="https://sperityventures.com/2012/06/the-seven-hills-of-richmond/" target="_blank" rel="noreferrer">Seven Hills of Richmond <FontAwesomeIcon icon={faExternalLinkAlt} /></a>
